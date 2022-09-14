3f4c40be9c6a79978cbee7af0706d332497d6326
Mayarabnc <mayarabncabral@gmail.com>
2022-09-14 13:01:37 -0300

    Modificações de design

--- front_devnology/src/app/cadastrar-link/cadastrar-link.component.ts
+++ front_devnology/src/app/cadastrar-link/cadastrar-link.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { LinkModel } from '../model/LinkModel';
@@ -18,11 +19,17 @@ export class CadastrarLinkComponent implements OnInit {
   listaDeLinks: LinkModel[]
 
   constructor(
-    private linkService: LinkService
+    private linkService: LinkService,
+    private router: Router
   ) { }
 
   ngOnInit(){
     window.scroll(0,0)
+
+    if(environment.token == ""){
+      alert("Sua seção expirou, faça o login novamente")
+      this.router.navigate(["/login"])
+    }
 
     this.findAllLinks()
   }
