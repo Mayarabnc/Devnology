a4234152ebc703df73a8e87a65e7853edbb23d66
Mayarabnc <mayarabncabral@gmail.com>
2022-09-14 11:32:35 -0300

    Modificação da pasta devnology

--- apiDevnology/karma.conf.js
+++ apiDevnology/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/devnology-front'),
+      dir: require('path').join(__dirname, './coverage/api-devnology'),
       subdir: '.',
       reporters: [
         { type: 'html' },
